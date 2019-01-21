489f346d0f5140ce04d36eebc704d4d6d7312e70
yaosaisai96 <1203845674@qq.com>
2019-01-21 09:48:24 +0800

    提交文件

--- bin/fy.js
+++ bin/fy.js
@@ -10,15 +10,6 @@ const configPath = path.resolve(__dirname, '../config.json');
 const config = JSON.parse(fs.readFileSync(configPath));
 const package = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json')));
 
-program
-  .usage(`\r\n  ${package.description}\r\n  GithubID: ${package.author}\r\n  Repository: ${package.repository.url}`)
-  .option('-v, --version', 'show version')
-  .option('-f, --baidu-from <from>', 'baidu engine: from language, default: auto')
-  .option('-t, --baidu-to <to>', 'baidu engine: to language, default: zh')
-  .option('-l, --baidu-language', 'baidu engine language')
-  .option('-e, --engine <engine>', 'change translate engine')
-  .option('-c, --config', 'show config.json')
-  .parse(process.argv);
 
 // baidu-engine language: from
 if (program.baiduFrom) {
@@ -61,21 +52,6 @@ else if (program.baiduLanguage) {
   let language = config.baidu.language;
   console.log('\r\n You can use:\r\n');
   console.log(language);
-}
-
-// change engine
-else if (program.engine) {
-  if (program.engine !== 'default' && (program.engine in config)) {
-    config.default = program.engine;
-    fs.writeFile(configPath, JSON.stringify(config));
-    console.log('success!');
-    return;
-  }
-  delete config.default;
-  console.log(`Error!\r\n  ~ The ${program.engine} is not exsit! \r\n  ~ Your input should be one of: `);
-  for (let item in config) {
-    console.log(`   ~  ${item}`);
-  }
 }
 
 // show config.json
